7c2efdbfb77b1e2b7f9c6b9f00158a4a8635b981
twistxxl <glade1337@mail.ru>
2025-08-22 19:33:53 +0300

    add jest test

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-export function someFn(arg: number): string {
-    const number = 12342342
-    const logRes = "Random" + number
-    console.log(logRes);
-    return `Random ${arg * number}`;
-}
-
-
-document.body.innerHTML = `<h1>${someFn(3)}</h1>`
