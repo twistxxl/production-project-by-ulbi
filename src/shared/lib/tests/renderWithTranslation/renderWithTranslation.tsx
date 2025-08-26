5f0929f1e9da039baa9d7415d55c3a43b364bb7b
twistxxl <glade1337@mail.ru>
2025-08-26 21:35:50 +0300

    fix sidebar test enderWithTranslation

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,11 +1,15 @@
-import { render } from "@testing-library/react";
-import { ReactNode } from "react";
-import { I18nextProvider } from "react-i18next";
-import i18nForTests from "shared/config/i18n/i18nForTests";
-
+import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18nForTests from 'shared/config/i18n/i18nForTests';
+import { BrowserRouter } from 'react-router-dom';
 
 export function renderWithTranslation(component: ReactNode) {
-    return render (
-        <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-    )
+    return render(
+        <BrowserRouter>
+            <I18nextProvider i18n={i18nForTests}>
+                {component}
+            </I18nextProvider>
+        </BrowserRouter>
+    );
 }
--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,31 +1,35 @@
-import { classNames } from "shared/lib/classNames/classNames"
-import stl from './AppLink.module.scss'
-import { Link, LinkProps } from "react-router-dom"
-import { FC } from "react"
-
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import { FC } from 'react';
+import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
     PRIMARY = 'primary',
-    SECONDARY = 'secondary'
+    SECONDARY = 'secondary',
+    RED = 'red',
 }
 
 interface AppLinkProps extends LinkProps {
-    className?: string
-    theme?: AppLinkTheme
+    className?: string;
+    theme?: AppLinkTheme;
 }
 
-
 export const AppLink: FC<AppLinkProps> = (props) => {
-
-    const { className, children, to, theme = AppLinkTheme.PRIMARY, ...otherProps } = props
+    const {
+        to,
+        className,
+        children,
+        theme = AppLinkTheme.PRIMARY,
+        ...otherProps
+    } = props;
 
     return (
         <Link
             to={to}
-            className={classNames(stl.appLink, {}, [className, stl[theme]])}
+            className={classNames(cls.AppLink, { [cls[theme]]: true }, [className])}
             {...otherProps}
         >
             {children}
         </Link>
-    )
-}
+    );
+};
