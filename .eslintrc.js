fe584e21556856fce367cb65e50d853de4e02979
twistxxl <glade1337@mail.ru>
2025-09-13 18:13:55 +0300

    add createSlice script

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,7 +68,7 @@ module.exports = {
         'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
         'no-param-reassign': 'off',
         'no-undef': 'off',
-        'twistxxl-plugin/path-checker': 'error',
+        'ulbi-tv-plugin/path-checker': 'error',
     },
     globals: {
         __IS_DEV__: true,
