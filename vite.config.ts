743f40ddd256b612eb8d1fb94158c5aadc07ebf2
twistxxl <glade1337@mail.ru>
2025-09-19 17:56:51 +0300

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        svgr({ exportAsDefault: true }),
+        react(),
+    ],
+    build: {
+        target: 'ESNext',
+    },
+    esbuild: {
+        target: 'ESNext',
+    },
+    resolve: {
+        alias: [
+            { find: '@', replacement: '/src' },
+        ],
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('http://localhost:8000'),
+        __PROJECT__: JSON.stringify('frontend'),
+    },
+});
